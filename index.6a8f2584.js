050633db06f34a0a3e131a543727c2f1a6a407db
Andrey Bondar <andrey_bondar@outlook.com>
2023-12-11 18:36:38 +0200

    make build

---
+++ index.6a8f2584.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-shadow */var e=document.querySelector(".gallery"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.getAttribute("src");t.setAttribute("src",r)});//# sourceMappingURL=index.6a8f2584.js.map
+
+//# sourceMappingURL=index.6a8f2584.js.map
